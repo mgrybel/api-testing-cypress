95f72c1d6393e5c219a31f3619f703c9ac337da1
mgrybel <grybel.m.m@gmail.com>
2024-06-29 19:27:25 +0200

    Added product tests

--- cypress/e2e/products.cy.js
+++ cypress/e2e/products.cy.js
@@ -1,9 +1,42 @@
+import { productSchema } from '../schemas/product';
+import { productsSchema } from '../schemas/products';
+
 describe('API Testing', () => {
-  it('get all products', () => {
-    cy.request('GET', 'https://jsonplaceholder.typicode.com/todos').then(
-      (response) => {
+  const USER = {
+    email: 'customer@test.com',
+    password: 'test123',
+    accessToken: '',
+  };
+
+  before(() => {
+    // root-level hook
+    // runs once before all tests
+    const response = cy
+      .request({
+        method: 'POST',
+        url: 'auth/login',
+        body: {
+          email: USER.email,
+          password: USER.password,
+        },
+      })
+      .then((response) => {
         expect(response.status).to.equal(200);
-      }
-    );
+        const responseBody = response.body;
+        USER.accessToken = responseBody.token;
+      });
+  });
+
+  it('get all products', () => {
+    cy.request({
+      method: 'GET',
+      url: 'products',
+      headers: {
+        Authorization: `Bearer ${USER.accessToken}`,
+      },
+    }).then((response) => {
+      expect(response.status).to.equal(200);
+      expect(response.body).to.be.jsonSchema(productsSchema);
+    });
   });
 });
